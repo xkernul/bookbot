33f2ec6a83aa2990eaa0e66752fae7aa39fd5fb0
xkernul <22508537+xkernul@users.noreply.github.com>
2025-05-26 19:12:49 -0400

    Added Stats, moved function from main

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+from stats import get_word_count
+
 
 def main():
     book_path = "books/frankenstein.txt"  # Relative path
     text = get_book_text(book_path)
-    print(text)
+    num_words = get_word_count(text)
+    print(f"{num_words} words found in the document")
 
 main()
+
+
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_wrd_count(text):
+    words = text.split()
+    return len(words)
